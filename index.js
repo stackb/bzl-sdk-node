e1e57e19256ba8820c83cb96e8a2baa6f11e18ae
Paul Johnston <paul.johnston02@sap.com>
2020-07-21 16:49:31 -0600

    Fix test deadline timeout

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const pb = require('./build/stack/bzl/v1beta1/application_pb');
 const grpc_pb = require('./build/stack/bzl/v1beta1/application_grpc_pb');
 
 module.exports = {
-    v1beta1: Object.assign(pb, grpc_pb),
+    v1beta1: Object.assign({}, pb, grpc_pb),
 };
